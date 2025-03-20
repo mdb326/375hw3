d60473504066b7f2634a9e15759c7d5fae1e5e53
Matthew Bergin <mdb326@iapetus.cse.lehigh.edu>
2025-03-19 20:34:59 -0400

    Starter code

---
+++ makefile
@@ -0,0 +1,5 @@
+hw: 
+	g++ -std=c++17 -o sequential sequential.cpp -O3
+
+clean: 
+	rm sequential
---
+++ sequential.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Entry point of the program
+int main() {
+    // Print a message to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0; // Indicate successful execution
+}
