df38b6a730c6fd68487332fd85d36c7e0fa767de
Matthew Bergin <mdb326@saturn.cse.lehigh.edu>
2025-03-24 15:30:39 -0400

    Run with the correct amount of adds, removes, contains

--- test.cpp
+++ test.cpp
@@ -1,18 +1,40 @@
 #include "sequential.h"
-#define SIZE 1024
+#define SIZE 8192
+#define TESTAMT 2000000
 
 int generateRandomVal();
+int generateRandomInteger(int min, int max);
 
 int main() {
     SequentialCuckoo<int> cuckoo(SIZE);
     cuckoo.populate(SIZE/2, generateRandomVal);
+    for(int i = 0; i < TESTAMT; i++){
+        int num = generateRandomInteger(1, 10);
+        if(num <= 8){
+            cuckoo.contains(cuckoo.contains(generateRandomVal()));
+        }
+        else if (num == 9){
+            cuckoo.add(generateRandomVal());
+        }
+        else {
+            cuckoo.remove(generateRandomVal());
+        }
+    }
     std::cout << cuckoo.size() << std::endl;
+
     return 0;
 }
 int generateRandomVal() {
     static std::random_device rd; // creates random device (unique to each thread to prevent race cons) (static to avoid reinitialization)
     static std::mt19937 gen(rd());  // Seeding the RNG (unique to each thread to prevent race cons) (static to avoid reinitialization)
-    std::uniform_int_distribution<> distrib(0, SIZE); // Create uniform int dist between min and max (inclusive)
+    std::uniform_int_distribution<> distrib(1, SIZE); // Create uniform int dist between min and max (inclusive)
+
+    return distrib(gen); // Generate random number from the uniform int dist (inclusive)
+}
+int generateRandomInteger(int min, int max) {
+    static std::random_device rd; // creates random device (unique to each thread to prevent race cons) (static to avoid reinitialization)
+    static std::mt19937 gen(rd());  // Seeding the RNG (unique to each thread to prevent race cons) (static to avoid reinitialization)
+    std::uniform_int_distribution<> distrib(min, max); // Create uniform int dist between min and max (inclusive)
 
     return distrib(gen); // Generate random number from the uniform int dist (inclusive)
 }
